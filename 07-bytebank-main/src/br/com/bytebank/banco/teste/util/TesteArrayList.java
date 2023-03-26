b1ae40fcc61ead98064fe2f0da7321fe586e45cf
hugosilva-dev <eahugosilva@gmail.com>
2023-03-26 00:06:19 -0300

    Update TesteArrayList
    Finalizando aula 04 do curso java.util

--- 07-bytebank-main/src/br/com/bytebank/banco/teste/util/TesteArrayList.java
+++ 07-bytebank-main/src/br/com/bytebank/banco/teste/util/TesteArrayList.java
@@ -1,6 +1,7 @@
 package br.com.bytebank.banco.teste.util;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
@@ -10,7 +11,7 @@ public class TesteArrayList {
 	public static void main(String[] args) {
 		
 		//Generics = especificação das listas, tornando desnecessario o cast
-		ArrayList<Conta> lista = new ArrayList<>();
+		List<Conta> lista = new ArrayList<>();
 		
 		ArrayList<String> nomes = new ArrayList<>();
 		
