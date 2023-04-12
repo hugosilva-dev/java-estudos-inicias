8c89d0c622662a1883b4effda3c5e6c54757354e
hugosilva-dev <eahugosilva@gmail.com>
2023-04-12 19:07:43 -0300

    Continuando os estudos do java.io

---
+++ java-io/src/br/com/alura/java/io/teste/TesteEscrita.java
@@ -0,0 +1,24 @@
+package br.com.alura.java.io.teste;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+
+public class TesteEscrita {
+
+	public static void main(String[] args) throws IOException {
+		
+		OutputStream fos = new FileOutputStream("resumo2.txt");
+		Writer osw = new OutputStreamWriter(fos);
+		BufferedWriter bw = new BufferedWriter(osw);
+
+		bw.write("Estudando comandos de saída com a linguagem de programação Java!");
+		bw.newLine();
+		bw.write("Isso tudo antes do café da manhã, Carol ainda está dormindo!");
+		
+		bw.close();
+	}
+}
